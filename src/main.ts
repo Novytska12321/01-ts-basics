4a6180814f7600ac029eb46b69b2c9faa4b614f9
Novytska12321 <novytska@seznam.cz>
2025-08-25 19:37:49 +0200

    tasks

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import './style.css'
+import './task-1'
+import './task-2'
+import './task-3'
+import './task-4'
+import './task-5'
+import './task-6'
+import './task-7'
+import './task-8'
+import viteLogo from '/vite.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+
